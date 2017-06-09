83a916df84a565b62375973e87af507177a1d5f2
Sergey Vilgodskiy <vilgodskiy@lenoblgis.ru>
2017-06-09 20:46:49 +0300

    Мелкие изменения

--- app/src/main/java/com/happyhamster/soft/gotodream/view/StartActivity.java
+++ app/src/main/java/com/happyhamster/soft/gotodream/view/StartActivity.java
@@ -1,4 +1,4 @@
-package com.happyhamster.soft.gotodream;
+package com.happyhamster.soft.gotodream.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.happyhamster.soft.gotodream.R;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -19,13 +21,8 @@ public class StartActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 
     @Override
